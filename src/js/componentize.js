e6e453c9f1dbc4c1b996c7a4a8875d2e16fa814c
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-01-15 21:50:57 +0100

    Wrap in `DOMDontentLoaded` event

--- src/js/componentize.js
+++ src/js/componentize.js
@@ -1,12 +1,14 @@
 import tableOfContents from './toc'
 import createFilter from './filter'
 
-const componentsWrapper = document.querySelector('.components')
+window.addEventListener('DOMContentLoaded', () => {
+	const componentsWrapper = document.querySelector('.components')
 
-if (componentsWrapper) {
-	const filter = createFilter(componentsWrapper)
-	const components = componentsWrapper.querySelectorAll('.component')
-	const toc = tableOfContents(Array.from(components))
-	componentsWrapper.appendChild(toc)
-	componentsWrapper.insertBefore(filter, componentsWrapper.firstElementChild)
-}
+	if (componentsWrapper) {
+		const filter = createFilter(componentsWrapper)
+		const components = componentsWrapper.querySelectorAll('.component')
+		const toc = tableOfContents(Array.from(components))
+		componentsWrapper.appendChild(toc)
+		componentsWrapper.insertBefore(filter, componentsWrapper.firstElementChild)
+	}
+})
