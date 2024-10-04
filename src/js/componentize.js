7f04a26a354d8e5ea4e25f7f3d978814d7c92e97
Chriztian Steinmeier <greystate@users.noreply.github.com>
2024-10-04 12:34:26 +0200

    Fix problems with using a container
    The TOC was no longer fixed in the viewport (a closer ancestor with a `container` property will become the containing block)

--- src/js/componentize.js
+++ src/js/componentize.js
@@ -3,12 +3,13 @@ import createFilter from './filter'
 
 window.addEventListener('DOMContentLoaded', () => {
 	const componentsWrapper = document.querySelector('.components')
+	const bodyElement = document.querySelector('body')
 
 	if (componentsWrapper) {
 		const filter = createFilter(componentsWrapper)
 		const components = componentsWrapper.querySelectorAll('.component')
 		const toc = tableOfContents(Array.from(components))
-		componentsWrapper.appendChild(toc)
 		componentsWrapper.insertBefore(filter, componentsWrapper.firstElementChild)
+		bodyElement.appendChild(toc)
 	}
 })
